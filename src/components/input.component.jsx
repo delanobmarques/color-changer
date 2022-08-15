5de122673540453f8d3aae146497a517f3f1a9f8
delanobmarques <delanobmarques@gmail.com>
2022-08-15 13:21:09 -0300

    add isDarkText to App's state, add button to toogle font color

--- src/components/input.component.jsx
+++ src/components/input.component.jsx
@@ -1,7 +1,7 @@
 import colorNames from "colornames";
 // colorName takes the name of the color and translate it to the corresponding  hex value
 
-const Input = ({ colorValue, setColorValue, setHexValue }) => {
+const Input = ({ colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText }) => {
   return (
     <form onSubmit={ (e) => e.preventDefault }>
         <label>Add color name: </label>
@@ -16,6 +16,12 @@ const Input = ({ colorValue, setColorValue, setHexValue }) => {
                 setHexValue(colorNames(e.target.value));
             }}
         />
+        <button
+            type="button" /*Since there's only one button the form assumes it's a submit. Define type button to avoid that */
+            onClick={() => setIsDarkText(!isDarkText)} /* Set isDarkText to the oposite of it's current value (true|false)*/
+        >
+            Toogle text color
+        </button>
         
     </form>
   )
--- src/components/square.component.jsx
+++ src/components/square.component.jsx
@@ -1,8 +1,11 @@
-const Square = ({ colorValue, hexValue }) => {
+const Square = ({ colorValue, hexValue, isDarkText }) => {
   return (
     <section 
         className="square" 
-        style={{ backgroundColor: colorValue}}
+        style={{ 
+            backgroundColor: colorValue,
+            color: isDarkText ? "#000" : "#FFF"
+        }}
     >
         <p>{ colorValue? colorValue : "Empty Value" }</p>
         <p>{ hexValue ? hexValue : null}</p>        
